814ef1c9d23b0a0d4d0f2532a76b0e1332fbe55f
Lyfthis <yihfoo@gmail.com>
2018-10-15 19:28:13 +0800

    Updated app.js to include default html page

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 
+//GET-s the default.html as the main page to which the app returns to
+app.get('/', function(req,res){
+	console.log('default html loading');
+	res.render('./public/index.html');
+});
+
 app.listen(port, function () {
     console.log('Started the Node.js server for this sample. Navigate to http://localhost:'+port+'/ to view the webpage.');
 });
